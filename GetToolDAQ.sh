bebf3eea9df048dd075bf47d380d6a46ead9f943
Evgenii Zhemchugov <evgenii.zhemchugov@cern.ch>
2023-02-17 13:51:45 +0600

    GetToolDAQ.sh: boost: do not build Boost.Python
    Fails to compile against python-3.10.9, and not used in this project.

--- GetToolDAQ.sh
+++ GetToolDAQ.sh
@@ -138,7 +138,7 @@ then
     rm -rf INSTALL    
     mkdir install 
     
-    ./bootstrap.sh --prefix=`pwd`/install/  > /dev/null 2>/dev/null
+    ./bootstrap.sh --prefix="`pwd`/install/" --without-libraries=python > /dev/null 2>/dev/null
     ./b2 install iostreams -j $threads
     
     export LD_LIBRARY_PATH=`pwd`/install/lib:$LD_LIBRARY_PATH
