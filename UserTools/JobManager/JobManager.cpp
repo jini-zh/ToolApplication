fa3872cc0b646477fdf0c50dcbf6749ede8e14dd
Eugene Zhemchugov <evgenii.zhemchugov@warwick.ac.uk>
2024-09-02 19:06:25 +0100

    JobManager: fix typos in configuration parameters
    Rename
    tread_cap        -> thread_cap
    global_tread_cap -> global_thread_cap

--- UserTools/JobManager/JobManager.cpp
+++ UserTools/JobManager/JobManager.cpp
@@ -10,8 +10,8 @@ bool JobManager::Initialise(std::string configfile, DataModel &data){
   //m_variables.Print();
 
  if(!m_variables.Get("verbose",m_verbose)) m_verbose=1;
- if(!m_variables.Get("tread_cap",m_thread_cap)) m_thread_cap=14;
- if(!m_variables.Get("global_tread_cap",m_data->thread_cap)) m_data->thread_cap=14;
+ if(!m_variables.Get("thread_cap",m_thread_cap)) m_thread_cap=14;
+ if(!m_variables.Get("global_thread_cap",m_data->thread_cap)) m_data->thread_cap=14;
  
  m_data->thread_num=0;
  
